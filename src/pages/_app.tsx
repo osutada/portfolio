73d3589eb30de85fe6f22a114f96f340f64afe8c
osutada <develop@osutada.jp>
2024-10-31 23:57:52 +0900

    ChakraUI導入/ ExtendTheme触りだけ記述

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body": {
+        background: "#0a0a0a",
+        color: "#ffffff",
+      },
+      a: {
+        color: "#ffffff",
+      },
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
+
+export default App;
